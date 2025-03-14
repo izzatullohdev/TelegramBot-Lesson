11973ddb86aac8e578f6319b4e2bb2b34271b71c
izzatullohdev <izzatbekmadaminov7141@gmail.com>
2025-03-14 23:48:21 +0500

    inline keyboard

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const TelegramBot = require("node-telegram-bot-api");
-const token = "7692526808:AAEXF3_pceGDw2wY1b26pXWicgUrUIISBII";
+const token = process.env.TOKEN;
 const bot = new TelegramBot(token, { polling: true });
 
 // 1-qadam
@@ -10,9 +10,41 @@ const bot = new TelegramBot(token, { polling: true });
 // });
 
 // 2-qadam
+// bot.onText(/\/start/, (message) => {
+//   bot.sendMessage(
+//     message.chat.id,
+//     `Assalomu alaykum ${message.chat.first_name} `
+//   );
+// });
+// 3-dars
+// inline_keyboard
 bot.onText(/\/start/, (message) => {
   bot.sendMessage(
     message.chat.id,
-    `Assalomu alaykum ${message.chat.first_name}`
+    `Assalomu alaykum ${message.chat.first_name} Pastdagi tugmalardan birini tanlang`,
+    {
+      reply_markup: {
+        inline_keyboard: [
+          [
+            {
+              text: "Google",
+              url: "https://google.com",
+            },
+            {
+              text: "Bio bot",
+              callback_data: "bio_bot",
+            },
+          ],
+        ],
+      },
+    }
   );
+});
+
+// callback_query
+bot.on("callback_query", (query) => {
+  const chatId = query.message.chat.id;
+  if (query.data === "bio_bot") {
+    bot.sendMessage(chatId, "Ushbu bot dasgasa dasturchi tomonidan yaratilgan");
+  }
 });
