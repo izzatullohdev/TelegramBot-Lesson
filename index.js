af6bca2d33335e9c18178dc9e8a92f7d91c4f12b
izzatullohdev <izzatbekmadaminov7141@gmail.com>
2025-03-19 21:24:32 +0500

    location

--- index.js
+++ index.js
@@ -2,64 +2,17 @@ require("dotenv").config();
 const TelegramBot = require("node-telegram-bot-api");
 const token = process.env.TOKEN;
 const bot = new TelegramBot(token, { polling: true });
-const axios = require("axios");
 
-bot.onText(/\/start/, async (msg) => {
-  const chatId = msg.chat.id;
-  try {
-    const response = await axios.get(
-      "https://jsonplaceholder.typicode.com/users"
-    );
-    const users = response.data;
-    let buttons = users.map((user) => [
-      {
-        text: user.name,
-        callback_data: `user_${user.id}`,
-      },
-    ]);
-    bot.sendMessage(chatId, "Foydalanuvchi tanlang", {
-      reply_markup: {
-        inline_keyboard: buttons,
-      },
-    });
-  } catch (err) {
-    bot.sendMessage(chatId, "Api Bilan bog'lanishda xatolik yuz berdi");
-  }
+bot.on(/\/start/, (msg) => {
+  bot.sendMessage(msg.chat.id, "iltimos locationingizni yuboring", {
+    reply_markup: {
+      keyboard: [[{ text: "location", request_location: true }]],
+      resize_keyboard: true,
+      one_time_keyboard: true,
+    },
+  });
 });
-
-bot.on("callback_query", async (query) => {
-  const chatId = query.message.chat.id;
-
-  if (query.data.startsWith("user_")) {
-    const userId = query.data.split("_")[1];
-    try {
-      const response = await axios.get(
-        `https://jsonplaceholder.typicode.com/users/${userId}`
-      );
-      const user = response.data;
-
-      bot.editMessageText(
-        `👤 *Ismi:* ${user.name}\n📧 *Email:* ${user.email}\n🏢 *Kompaniya:* ${user.company.name}`,
-        {
-          chat_id: chatId,
-          message_id: query.message.message_id,
-          parse_mode: "Markdown",
-          reply_markup: {
-            inline_keyboard: [
-              [
-                {
-                  text: "Boshqa foydalanuvchini tanlash",
-                  callback_data: "/start",
-                },
-              ],
-            ],
-          },
-        }
-      );
-    } catch (err) {
-      bot.sendMessage(chatId, "Ma'lumotlar olishda xatolik yuz berdi");
-    }
-  }
-
-  bot.answerCallbackQuery(query.id);
+bot.on("location", (msg) => {
+  const { latitude, longitude } = msg.location;
+  bot.sendMessage(msg.chat.id, `latitude: ${latitude}, longitude: ${longitude}`);
 });
